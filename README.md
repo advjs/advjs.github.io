e7dd8df7489802f9df6640c993becae92c78a7aa
YunYouJun <me@yunyoujun.cn>
2020-10-02 05:55:29 +0800

    :tada: init with vite

---
+++ README.md
@@ -0,0 +1,5 @@
+# advjs.github.io
+
+> adv.js.org
+
+这是一个还在咕咕咕的主页。
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <h2>面向未来前端的 ADV 文字冒险游戏引擎</h2>
+  <a href="https://adv.yunyoujun.cn/docs/" target="_blank">
+    <button class="zi-btn">Docs</button>
+  </a>
+  <a href="https://github.com/YunYouJun/adv" target="_blank">
+    <button class="zi-btn primary">
+      GitHub
+      <i class="prefix zi-icon-github"></i>
+    </button>
+  </a>
+  <a href="https://adv.yunyoujun.cn/parser/" target="_blank">
+    <button class="zi-btn">Parser</button>
+  </a>
+  <h3 style="margin-top: 3rem">愿能在未来的冒险中，与你相遇</h3>
+  <h3 style="margin-top: 2rem">开发中，敬请期待</h3>
+  <h3>...</h3>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+button {
+  margin: 5px;
+}
+</style>
